81934a9373ce4ea9ec573e9e87480bb042185754
Aditya Yadav <aditya.yadav@dhwaniris.comm>
2024-02-07 01:53:24 +0530

    Menu toggle to

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'CRM';
+export class AppComponent implements DoCheck {
+  isMenuRequire = false;
+  constructor(
+    private router: Router
+  ){}
+  ngDoCheck(): void {
+      let currentUrl = this.router.url;
+      if(['/login','/register'].includes(currentUrl)){
+        this.isMenuRequire = false;
+      } else {
+        this.isMenuRequire = true;
+      }
+  }
 }
