9472520668338fbde64c3a51f0e0f0f24e9d29ea
Aditya Yadav <aditya.yadav@dhwaniris.comm>
2024-02-06 17:47:11 +0530

    added auth service function and created registration reactive form

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  baseAPI = 'localhost:3000/user'
+  constructor( private http: HttpClient) { };
 
-  constructor() { }
+  getAll(){
+    return this.http.get(this.baseAPI);
+  }
+
+  getById(id:string){
+    return this.http.get(this.baseAPI+'/'+id);
+  }
+
+  registerData(inputData: any){
+    return this.http.post(this.baseAPI, inputData);
+  }
+
+  updateData(id: string, updateData: any){
+    return this.http.put(this.baseAPI+'/'+id,updateData);
+  }
+
+  
 }
